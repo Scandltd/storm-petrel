9c7d3eaf234e8da0db9149c3f6981b23b1694c3b
Andrei Samsonov <sam@scand.com>
2025-01-20 07:35:11 +0000

    Fix sign.ps1 to avoid NuGet packages failure on Unix-like platforms

--- build/sign.ps1
+++ build/sign.ps1
@@ -22,15 +22,44 @@ $nupkgUnzippedPath = "$directoryPathForResult/nupkg-unzipped"
 New-Item -Path "$directoryPathForResult" -ItemType Directory
 New-Item -Path "$nupkgUnzippedPath" -ItemType Directory
 
-Write-Output "Unzipping to $nupkgUnzippedPath"
-Copy-Item -Path "$NupkgFilePath" -Destination "$directoryPathForResult/tmp.zip"
-Expand-Archive -LiteralPath "$directoryPathForResult/tmp.zip" -DestinationPath $nupkgUnzippedPath
-Remove-Item -Path "$directoryPathForResult/tmp.zip"
+Write-Output "Copying to $directoryPathForResult"
+Copy-Item -Path "$NupkgFilePath" -Destination "$directoryPathForResult/$fileName"
 
-$filesToSign = Get-ChildItem -Path $nupkgUnzippedPath -Filter "Scand.StormPetrel.*.dll" -File -Recurse
-foreach ($fileToSign in $filesToSign) {
-    $fileToSignFullPath = "$($fileToSign.FullName)"
-    if (-not $isAbstractionFileName -and $fileToSign.Name -eq "Scand.StormPetrel.Generator.Abstraction.dll") {
+$relativePaths = [System.Collections.ArrayList]::new()
+$zip = [System.IO.Compression.ZipFile]::OpenRead("$directoryPathForResult/$fileName")
+
+foreach ($entry in $zip.Entries) {
+    Write-Output $entry.FullName
+    if (!($entry.Name -match "Scand\.StormPetrel\..*dll")) {
+        continue
+    }
+    $extractFileRelativePath = $entry.FullName
+    #Ensure Unix-like path
+    if ($extractFileRelativePath -match "\\") {
+        Write-Error "Unexpected back slash in $extractFileRelativePath"
+        $zip.Dispose()
+        exit 1
+    }
+    $relativePaths.Add($extractFileRelativePath)
+    $entryToExtract = $zip.GetEntry($extractFileRelativePath)
+    if ($extractFileRelativePath.Length -eq $entry.Name.Length) {
+        Write-Error "Unexpected case: Scand.StormPetrel dll is in the root"
+        $zip.Dispose()
+        exit 1
+    }
+    $extractFileRelativeDir = $extractFileRelativePath.Substring(0, $extractFileRelativePath.Length - $entry.Name.Length)
+    $extractFileDir = "$nupkgUnzippedPath\$extractFileRelativeDir"
+    if (!([System.IO.Directory]::Exists($extractFileDir))) {
+        Write-Output "Creating directory $extractFileDir"
+        [System.IO.Directory]::CreateDirectory($extractFileDir)
+    }
+    [System.IO.Compression.ZipFileExtensions]::ExtractToFile($entryToExtract, "$nupkgUnzippedPath\$extractFileRelativePath")
+}
+$zip.Dispose()
+
+foreach ($relativePath in $relativePaths) {
+    $fileToSignFullPath = "$nupkgUnzippedPath/$relativePath"
+    if ($relativePath.EndsWith("Scand.StormPetrel.Generator.Abstraction.dll")) {
         & "$SignToolPath" verify /pa "$fileToSignFullPath"
         if ($LASTEXITCODE -ne 0) {
             Write-Error "$fileToSignFullPath must already be signed in context of building of $fileName"
@@ -46,10 +75,24 @@ foreach ($fileToSign in $filesToSign) {
     }
 }
 
-Write-Output "Zipping new nuget package"
+Write-Output "Re-packing new nuget package"
 $newFilePath = "$directoryPathForResult/$fileName"
-Compress-Archive -Path "$nupkgUnzippedPath/*" -DestinationPath "$newFilePath.zip"
-Move-Item -Path "$newFilePath.zip" -Destination "$newFilePath"
+$zip = [System.IO.Compression.ZipFile]::Open($newFilePath, [System.IO.Compression.ZipArchiveMode]::Update)
+foreach ($relativePath in $relativePaths) {
+    if ($relativePath.EndsWith("Scand.StormPetrel.Generator.Abstraction.dll")) {
+        #Already signed, no need to re-archive
+        continue
+    }
+    foreach ($entry in $zip.Entries) {
+        if ($entry.FullName -eq $relativePath) {
+            $entryToDelete = $entry
+        }
+    }
+    $entryToDelete.Delete()
+    $fileToSignFullPath = "$nupkgUnzippedPath/$relativePath"
+    [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $fileToSignFullPath, $relativePath)
+}
+$zip.Dispose()
 
 Write-Output "Signing new nuget package"
 dotnet nuget sign "$newFilePath" --certificate-fingerprint $CertificateFingerprintForNugetSign --timestamper http://timestamp.globalsign.com/tsa/r6advanced1
