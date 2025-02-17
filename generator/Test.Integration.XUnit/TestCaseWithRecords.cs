80cf47912d64b1508efe377071489b978821a01f
Dmitry Prokopenko <prokopenko@scand.com>
2025-02-17 16:31:47 +0000

    Add tests for records

---
+++ generator/Test.Integration.XUnit/TestCaseWithRecords.cs
@@ -0,0 +1,31 @@
+﻿namespace Test.Integration.XUnit;
+
+public class TestCaseWithRecords
+{
+    [Fact]
+    public void AssertEqualWithRecordTest()
+    {
+        //Act
+        var actual = new TestedRecord(100);
+
+        //Assert
+        Assert.Equal(new TestedRecord(123), actual);
+    }
+
+    [Fact]
+    public void AssertEqualWithRecordAndRecordPropertyTest()
+    {
+        //Act
+        var actual = new TestedRecordWithProperty { Value = 100 };
+
+        //Assert
+        Assert.Equal(new TestedRecordWithProperty { Value = 123 }, actual);
+    }
+}
+
+public record TestedRecord(int Value);
+
+public record TestedRecordWithProperty
+{
+    public int Value { get; init; }
+}
