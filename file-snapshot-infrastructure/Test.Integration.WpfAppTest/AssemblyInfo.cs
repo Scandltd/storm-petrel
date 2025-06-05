ea59d79fd54b3068abf19c094fbf6a50e9c702e3
Andrei Samsonov <sam@scand.com>
2025-06-05 14:09:20 +0000

    Fix WPF tests instability

---
+++ file-snapshot-infrastructure/Test.Integration.WpfAppTest/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿// Disable test parallelization to avoid tests instability. The instability root cause is:
+// - `Test.Integration.WpfApp.MainWindow` constructor calls auto-generated `InitializeComponent` which calls `System.Windows.Application.LoadComponent` which is not thread-safe [1].
+// - Current test project has more than one test class (including StormPetrel generated classes) instantiating the `MainWindow`.
+// - The test classes are executed by xUnit in parallel by default what sometimes results in an exception due to `LoadComponent` thread-unsafety.
+// References:
+// [1] https://learn.microsoft.com/en-us/dotnet/api/system.windows.application.loadcomponent?view=windowsdesktop-9.0#system-windows-application-loadcomponent(system-object-system-uri)
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
