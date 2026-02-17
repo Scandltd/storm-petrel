c9b36d36034cd7821382dc46a7f61ee346159419
samsonovak8 <2715309@gmail.com>
2026-02-17 19:51:59 +0300

    fix

--- generator/Test.Integration.XUnit/EntityFrameworkExamples/ClassModels.cs
+++ generator/Test.Integration.XUnit/EntityFrameworkExamples/ClassModels.cs
@@ -1,21 +1,20 @@
 using System.Collections.ObjectModel;
 
-namespace Test.Integration.XUnit.EntityFrameworkExamples
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string Email { get; set; } = string.Empty;
-        public ICollection<Post> Posts { get; init; } = [];
-    }
+namespace Test.Integration.XUnit.EntityFrameworkExamples;
 
-    public class Post
-    {
-        public int Id { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public int UserId { get; set; }
-        public User? User { get; set; }
-        public int? ModeratorId { get; set; }
-        public User? Moderator { get; set; }
-    }
+public class User
+{
+    public int Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public ICollection<Post> Posts { get; init; } = [];
+}
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public int UserId { get; set; }
+    public User? User { get; set; }
+    public int? ModeratorId { get; set; }
+    public User? Moderator { get; set; }
 }
