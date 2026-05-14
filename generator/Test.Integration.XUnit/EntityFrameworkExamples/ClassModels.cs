96f38d4b286a52592d9e784d342058f32e515b13
Katya Samsonova <89536093+samsonovak8@users.noreply.github.com>
2026-05-14 13:29:55 +0300

    Entity framework examples

---
+++ generator/Test.Integration.XUnit/EntityFrameworkExamples/ClassModels.cs
@@ -0,0 +1,18 @@
+namespace Test.Integration.XUnit.EntityFrameworkExamples;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public ICollection<Post> Posts { get; init; } = [];
+}
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public int UserId { get; set; }
+    public User? User { get; set; }
+    public int? ModeratorId { get; set; }
+    public User? Moderator { get; set; }
+}
